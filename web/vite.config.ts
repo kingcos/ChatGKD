0608c3c72fca7e9484a4c87307a9119b2b62c8cc
kingcos <2821836721v@gmail.com>
2023-03-04 23:53:17 +0800

    Support PWA

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,3 +1,4 @@
+import { VitePWA } from "vite-plugin-pwa";
 import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
@@ -6,7 +7,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === "development" ? "" : "/ChatGKD/",
-  plugins: [vue()],
+  plugins: [vue(), VitePWA()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
